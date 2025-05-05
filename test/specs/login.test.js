b842a5ec2c6a12098040ea445ce781a90b0f72f0
leoli7405@gmail.com <leoli7405@gmail.com>
2025-05-05 12:51:02 -0700

    updated safari test

--- test/specs/login.test.js
+++ test/specs/login.test.js
@@ -1,8 +1,11 @@
 import FormPage from '../pageobjects/login.page.js';
 
 describe('Login Test', () => {
-    it('App should deny access with wrong credentials',  async () => {
+    before(async () => {
         await FormPage.open();
+    })
+
+    it('App should deny access with wrong credentials',  async () => {
         await FormPage.login({ username:'foo', password: 'bar!' });
         await FormPage.flash.waitForDisplayed({ timeout: 3000 });
         await expect(await FormPage.flash).toHaveText(
@@ -10,7 +13,6 @@ describe('Login Test', () => {
     });
 
     it('App should allow access with correct credentials',  async () => {
-        await FormPage.open();
         await FormPage.login({ username:'tomsmith', password: 'SuperSecretPassword!' });
         await FormPage.flash.waitForDisplayed({ timeout: 3000 });
         await expect(await FormPage.flash).toHaveText(
